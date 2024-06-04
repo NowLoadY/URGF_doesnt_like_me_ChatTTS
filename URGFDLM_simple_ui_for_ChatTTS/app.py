12fa6b55dc8f3683a244e05b436754d89a1cce27
Danny <79356856+NowLoadY@users.noreply.github.com>
2024-06-04 23:32:15 +0800

    Update app.py
    add find seed

--- URGFDLM_simple_ui_for_ChatTTS/app.py
+++ URGFDLM_simple_ui_for_ChatTTS/app.py
@@ -45,7 +45,7 @@ BoxLayout:
         TextInput:
             id: input_text
             size_hint: 1, 0.7
-            multiline: False
+            multiline: True
             font_name: 'msyh'
         BoxLayout:
             orientation: 'horizontal'
@@ -71,10 +71,17 @@ BoxLayout:
                 Switch:
                     id: switch3
                     active: False
-        Button:
-            id: submit_button
-            text: 'Generate'
+        BoxLayout:
+            orientation: 'horizontal'
             size_hint: 1, 0.1
+            Button:
+                id: submit_button
+                text: 'Generate'
+                size_hint: 0.5, 0.1
+            Button:
+                id: find_seed_button
+                text: 'find_seed'
+                size_hint: 0.5, 0.1
         BoxLayout:
             orientation: 'vertical'
             size_hint: 1, 0.1
@@ -117,6 +124,7 @@ class ChatApp(App):
         self.speaker_spinner = layout.ids.speaker_spinner
         self.input_text = layout.ids.input_text
         self.submit_button = layout.ids.submit_button
+        self.find_seed_button = layout.ids.find_seed_button
         self.switch1 = layout.ids.switch1
         self.switch2 = layout.ids.switch2
         self.audio_list = layout.ids.audio_list
@@ -126,6 +134,7 @@ class ChatApp(App):
         
         self.speaker_spinner.bind(text=self.on_speaker_select)
         self.submit_button.bind(on_press=self.infer_and_play)
+        self.find_seed_button.bind(on_press=self.start_finding_seed)
         self.switch1.bind(active=self.toggle_skip_refine_text)
         self.switch2.bind(active=self.toggle_use_decoder)
 
@@ -148,6 +157,7 @@ class ChatApp(App):
     def choose_speaker(self, seed=0):
         self.deterministic(seed)
         self.rnd_spk_emb = self.chat.sample_random_speaker()
+        self.custom_seed_input.text = str(seed)
 
     def adjust_pitch(self, audio_path, new_pitch=0):
         sound = AudioSegment.from_file(audio_path)
@@ -155,14 +165,24 @@ class ChatApp(App):
             "frame_rate": int(sound.frame_rate * (2.0 ** (new_pitch / 12.0)))
         }).set_frame_rate(sound.frame_rate)
         new_sound.export(audio_path, format="wav")
-    
-    def infer_and_play(self, instance=None):
+
+    def start_finding_seed(self, instance=None):
         custom_seed = self.custom_seed_input.text.strip()
         if custom_seed.isdigit():
-            self.choose_speaker(int(custom_seed))
+            initial_seed = int(custom_seed)
+            text = self.input_text.text
+            for i in range(100):
+                self.choose_speaker(initial_seed + i)
+                audio_path = self.infer(text)
+                self.play_audio(audio_path)
 
-        text = self.input_text.text
-        
+    def save_audio(self, wav, name, dir='generated_audios'):
+        temp_audio_file = tempfile.NamedTemporaryFile(dir=dir, delete=False, suffix=".wav", prefix=name)
+        sf.write(temp_audio_file, wav, 24000, format='WAV', subtype='PCM_24')
+        temp_audio_file.close()
+        return temp_audio_file.name
+    
+    def infer(self, text):
         if len(text) > self.max_word:
             sentences = re.split(r'(?<=[。！？;；.!?;,，])', text)
             sentences = [s.strip() for s in sentences if s.strip() != '']
@@ -191,13 +211,14 @@ class ChatApp(App):
 
             }
             params_refine_text = {
-            'prompt': '[oral_0]'
+            #'prompt': '[oral_0]'
             } 
             wav = self.chat.infer(sentence,
                                 params_refine_text=params_refine_text,
                                 params_infer_code=params_infer_code,
                                 use_decoder=self.use_decoder,
-                                skip_refine_text=self.skip_refine_text)[0][0]
+                                skip_refine_text=self.skip_refine_text,
+                                do_text_normalization=False)[0][0]
             combined_wav.append(wav)
 
         if len(combined_wav) > 1:
@@ -205,15 +226,21 @@ class ChatApp(App):
         else:
             combined_wav = combined_wav[0]
         safe_text = re.sub(r'[^\w]', '', self.input_text.text[:25])
-        temp_audio_file = tempfile.NamedTemporaryFile(dir='generated_audios', delete=False, suffix=".wav", prefix="seed"+str(self.using_seed)+"_"+safe_text+"_")
-        sf.write(temp_audio_file, combined_wav, 24000, format='WAV', subtype='PCM_24')
-        temp_audio_file.close()
-        self.play_audio(temp_audio_file.name)
+        audio_name = self.save_audio(wav=combined_wav, name="seed"+str(self.using_seed)+"_"+safe_text+"_", dir='generated_audios')
         button_text = "seed:{}_".format(self.using_seed) + self.input_text.text[:10] + "..."
-        self.add_audio_to_list(button_text, temp_audio_file.name)
+        self.add_audio_to_list(button_text, audio_name)
+        return audio_name
+
+    def infer_and_play(self, instance=None):
+        custom_seed = self.custom_seed_input.text.strip()
+        if custom_seed.isdigit():
+            self.choose_speaker(int(custom_seed))
+        text = self.input_text.text
+        audio_path = self.infer(text=text)
+        self.play_audio(audio_path)
 
     def play_audio(self, filename):
-        self.adjust_pitch(filename)
+        #self.adjust_pitch(filename)
         sound = SoundLoader.load(filename)
         if sound:
             sound.play()
